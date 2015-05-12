a3182b5fd8e29bdbf8920dd54f1d2cd66b6b7e7f
awarriorer <awarriorer@163.com>
2015-05-12 09:45:26 +0800

    MymusicPlayForMobile20150512

--- js/mainJs.js
+++ js/mainJs.js
@@ -1,9 +1,9 @@
 
 /*
 	声明：本项目仅仅用于学习和技术交流，如有商业目的的引用，请通知本项目作者
-	作者：小鑫鑫
+	作者：jack大叔
 	项目起始时间：2015年5月9日10:40:36
-	最后更新时间：2015年5月9日10:40:45
+	最后更新时间：2015年5月12日9:21:20
 
 	
 	本文件主要为本项目提供主要的逻辑代码支持，及主程序
@@ -21,6 +21,16 @@
 
 
 (function($){
+
+	// 随机背景
+	function randomBackground(){
+		var obj   = $('#pageA,#pageB'),
+			maxBg = 6;
+		obj.css({
+			'background-image':'url(images/pageBg/'+ parseInt(Math.random()*maxBg+1) +'.jpg)'
+		});
+	}
+	randomBackground();
 
 
 	//歌曲类型列表宽度高度样式初始化
@@ -42,6 +52,48 @@
 		});	
 	}
 	songListStyleInfo();
+
+
+	//页面总体样式的改变,上下，左右的切换
+	function showPage(){
+		var pageA       = $('#pageA'),
+			pageB       = $('#pageB'),
+			songClass   = $('#misicClassList').find('li'),//类列表
+			returnPageA = $('#returnPageA'),//B中返回A的按钮
+			pageBArr    = $('#pageBArr'),//b页面中，的三个显示集合
+			BnowPage    = $('#pageB_nowPage').find('span'),//三个小按钮
+			windowW     = $(document).width(),
+
+			//swiper,左右切换,记录原点
+			mySwiper    = new Swiper('.swiper-container',{
+				onSlideChangeEnd: function(swiper){
+			      	var index = swiper.snapIndex;
+			      	BnowPage.eq(index).addClass('active').siblings().removeClass('active');
+			    }
+			});
+			
+			songClass.tap(function(){
+				pageB.css({
+					'transform':'translate3D(0,0,0)',
+					'-webkit-transform':'translate3d(0,0,0)'
+				});
+				pageBArr.css({
+					'transform':'translate3D(0,0,0)',
+					'-webkit-transform':'translate3d(0,0,0)'
+				});
+				BnowPage.eq(0).addClass('active').siblings().removeClass('active');
+
+			});
+			returnPageA.tap(function(){
+				pageB.css({
+					'transform':'translate3D(0,100%,0)',
+					'-webkit-transform':'translate3d(0,100%,0)'
+				});
+			});
+
+
+	}
+	showPage();
 
 
 
